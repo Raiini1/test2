cd0c2232d00e167b14bab648930b34cd33bdf958
Raiini1 <olesyamolkovaa@icloud.com>
2024-12-07 14:59:48 +0300

    загрузка проекта

---
+++ test1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SQLite;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace test1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string Role;
+            string cs = @"Data Source=D:\\Рабочий стол\\test\\test\\test.db;Version=3;";
+            using (SQLiteConnection connection = new SQLiteConnection(cs))
+            {
+                connection.Open();
+                SQLiteCommand command = new SQLiteCommand("SELECT Role FROM Avtorizacia WHERE Login = @Login and Parol = @Parol", connection);
+                command.Parameters.AddWithValue("@Login", textBoxLogin.Text);
+                command.Parameters.AddWithValue("@Parol", TextBoxParol.Password);
+                Role = (string)command.ExecuteScalar();
+            }
+            if (Role == "Администратор")
+            {
+                this.Hide();
+                Prep f3 = new Prep();
+                f3.Show();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ test1/Prep.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace test1
+{
+    /// <summary>
+    /// Логика взаимодействия для Prep.xaml
+    /// </summary>
+    public partial class Prep : Window
+    {
+        private string cs = "Data Source=D:\\Рабочий стол\\test\\test\\test.db;Version=3;";
+        public Prep()
+        {
+            InitializeComponent();
+            Load_Prep();
+        }
+        private void Load_Prep()
+        {
+            List<Prepodavateli> prep = new List<Prepodavateli>();
+
+            using (SQLiteConnection connection = new SQLiteConnection(cs))
+            {
+                connection.Open();
+
+                string query = "SELECT ID_Prepodavatel, Familia, Imya, Otchestvo, Data_rojdenia, Adres_projivania, Doljnost.Nazvanie_doljnosti as Doljnost_ID FROM Prepodavatel JOIN Doljnost ON Prepodavatel.Doljnost_ID = Doljnost.ID_Doljnost";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Prepodavateli prepp = new Prepodavateli
+                        {
+                            ID_Prepodavatel = reader["ID_Prepodavatel"].ToString(),
+                            Familia = reader["Familia"].ToString(),
+                            Imya = reader["Imya"].ToString(),
+                            Otchestvo = reader["Otchestvo"].ToString(),
+                            Data_rojdenia = reader["Data_rojdenia"].ToString(),
+                            Adres_projivania = reader["Adres_projivania"].ToString(),
+                            Doljnost_ID = reader["Doljnost_ID"].ToString(),
+                        };
+                        prep.Add(prepp);
+                    }
+                }
+            }
+            DataGridPrep.ItemsSource = prep;
+        }
+    }
+
+    public class Prepodavateli
+    {
+        public string ID_Prepodavatel { get; set; }
+        public string Familia { get; set; }
+        public string Imya { get; set; }
+        public string Otchestvo { get; set; }
+        public string Data_rojdenia { get; set; }
+        public string Adres_projivania { get; set; }
+        public string Doljnost_ID { get; set; }
+    }
+}
